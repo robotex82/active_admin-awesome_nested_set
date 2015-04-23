58456a46f418129962a0edc64712b09ed064a747
Roberto Vasquez Angel <robotex@robotex.de>
2015-04-23 22:17:09 +0200

    Feature: Added rails 4.2 support.

--- lib/active_admin/views/index_as_nested_set.rb
+++ lib/active_admin/views/index_as_nested_set.rb
@@ -14,21 +14,31 @@ module ActiveAdmin
     class IndexAsNestedSet < IndexAsTable
 
       def build(page_presenter, collection)
-        super page_presenter, get_nested_set( resource_class )
+p Rails.version      
+        super page_presenter, get_nested_set(resource_class)
+#        if Rails.version < '4.0.0'
+#          super page_presenter, get_nested_set( resource_class )
+#        else
+#          super page_presenter, get_nested_set_rails_4( resource_class )
+#        end
       end
 
       protected
 
-      def get_nested_set class_or_item
-        result = []
+#      def get_nested_set(class_or_item)
+#        result = []
 
-        roots = class_or_item.respond_to?(:scoped) ? class_or_item.roots : class_or_items
+#        roots = class_or_item.respond_to?(:scoped) ? class_or_item.roots : class_or_item
 
-        items = roots.each do |root|
-          result += root.self_and_descendants.compact
-        end
+#        items = roots.each do |root|
+#          result += root.self_and_descendants.to_a.compact
+#        end
 
-        result
+#        result
+#      end
+
+      def get_nested_set(resource_class)
+        resource_class.roots.map(&:self_and_descendants).flatten
       end
 
     end
